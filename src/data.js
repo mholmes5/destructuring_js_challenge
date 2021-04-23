0982c4274067f1ba57f4d3f6fe5ac521d1331488
Michael Holmes <michaelholmes618@gmail.com>
2021-04-23 17:02:45 -0500

    detructuring objects

--- src/data.js
+++ src/data.js
@@ -1,4 +1,30 @@
 const animals = [
-  { name: "cat", sound: "meow" },
-  { name: "dog", sound: "woof" }
+  {
+    name: "cat",
+    sound: "meow",
+    feedingRequirements: {
+      food: 2,
+      water: 3
+    }
+  },
+  {
+    name: "dog",
+    sound: "woof",
+    feedingRequirements: {
+      food: 2,
+      water: 3
+    }
+  }
 ];
+
+function useAnimals(animal) {
+  return [
+    animal.name,
+    function () {
+      console.log(animal.sound);
+    }
+  ];
+}
+
+export default animals;
+export { useAnimals };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
-//CHALLENGE: uncomment the code below and see the car stats rendered
-import React from "react";
-import ReactDOM from "react-dom";
+import animals, { useAnimals } from "./data";
 
-ReactDOM.render(
-  <table>
-    <tr>
-      <th>Brand</th>
-      <th>Top Speed</th>
-    </tr>
-    <tr>
-      <td>{tesla.model}</td>
-      <td>{teslaTopSpeed}</td>
-      <td>{teslaTopColour}</td>
-    </tr>
-    <tr>
-      <td>{honda.model}</td>
-      <td>{hondaTopSpeed}</td>
-      <td>{hondaTopColour}</td>
-    </tr>
-  </table>,
-  document.getElementById("root")
-);
+//console.log(animals);
+const [cat, dog] = animals;
+
+const [animal, makeSound] = useAnimals(dog);
+
+//console.log(dog);
+
+// const { name, sound: dogSound } = dog;
+// console.log(dogSound);
+
+//const { name = "Spot", sound = "Grrr", age = 3 } = dog;
+//console.log(name + "  " + age);
+
+// const {
+//   name,
+//   sound,
+//   feedingRequirements: { food, water }
+// } = cat;
+
+// console.log(food);
+
+// //CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
